a21a077276fb14da7f5fb631239febe665e5e3d8
DANIEL PEREZ <c-daniel.perez@radarfirst.com>
2024-04-04 10:10:52 -0600

    Create academy repo

---
+++ selenium_tests/SeleniumTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace selenium_tests;
+
+public class SeleniumTest
+{
+    IWebDriver driver;
+
+    [SetUp]
+    public void start_Browser()
+    {
+        driver = new ChromeDriver();
+        driver.Manage().Window.Maximize();
+    }
+
+    [Test]
+    [Category("Google")]
+    public void firstTest()
+    {
+        driver.Navigate().GoToUrl("https://www.amazon.com.mx/");
+    }
+
+    [TearDown]
+    public void close_Browser()
+    {
+        driver.Quit();
+    }
+}
